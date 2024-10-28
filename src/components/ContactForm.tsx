5d4d30f755354753de690e2a90f9dc5a6088639a
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-28 02:47:59 -0700

    add contact

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styles from "./ContactForm.module.scss";
 
-
 const ContactForm: React.FC = () => {
   return (
     <section className="contact" id="contact">
       <div className={styles.container}>
-
         <h2>Contact Me</h2>
-        <form className="contact__form">
+        <form className={styles.form}>
           <input type="text" name="name" placeholder="Name" required />
           <input type="email" name="email" placeholder="Email" required />
           <textarea name="message" placeholder="Message" required></textarea>
-          <button type="submit">Send</button>
+          <button className={styles.btn} type="submit">
+            Send
+          </button>
         </form>
       </div>
     </section>
